106de74d08d0616cb124f68a1317b06dee8b1a18
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-08 16:05:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -96,9 +96,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => {
-            :alans_new_info => "GUI"
-          },
+          :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
